abe8124a64464fa069b0f7b2b941ff4251b883ee
Maegan Phan <maegyoung@gmail.com>
2023-10-20 11:33:10 -0500

    tutorial complete

--- main.py
+++ main.py
@@ -11,3 +11,35 @@ class Model(nn.Module): # Model class derives from nn.Module. Is used to keep tr
     def forward(self, input): #This method is our forward pass. When called, will go through the neural network and go from an input to an output.
         out = self.linear(input) # this layer is performing the y = xw + b. Linear transformation
         return out
+    
+#Create Model instance and set to a variable
+
+model = Model()
+mse = nn.MSELoss() #Loss generator. This is how we get our Loss. Loss compares output that we received, based on what was calculated, to our desired output. What's the difference? Prediction vs result and how close are they together? Let's us calculate the prediction accuracy of the model. Means^2Error, Loss
+
+optimizer = optim.SGD(model.parameters(), lr=0.01) # Going to update our parameters. Stotastic Gradient Descent (SGD). Optimizes parameters to get them to the lowest possible point on bell curve. lr is learning rate.
+
+#Sample inputs
+
+#Makes a 5 X 1 matrix
+inputs = torch.rand((5, 1), dtype=torch.float32) #float32 occupies 32 bits
+factor = 2  #multiplying all the numbers in matrix by 2
+targets = inputs * factor
+
+#Training Loop 
+#epoch = training iteration
+
+for epoch in range(10000):
+    #Forward pass
+    outputs = model.forward(inputs)
+    loss = mse(outputs, targets)
+
+    #Backward pass and optimization
+    optimizer.zero_grad() #zero-gradient accumulation. By default, pytorch will accumulate gradients. This is a best practice. Making sure gradient is only from current iteration, not from ones before. There are some instances where you would want to accumulate gradients for more complex models.
+    loss.backward()
+    optimizer.step() #able to update our model parameters; refined
+
+    if epoch % 100 == 0: # every 100 iterations, print the training iteration # and the loss
+        print(f'epoch {epoch} loss: {loss:.4f}')
+
+#to run in terminal, $ python main.py
